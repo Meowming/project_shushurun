ed69628407465490dc7af86188f1a849bec94d08
Meowming <demingz1@uci.edu>
2022-03-06 21:35:29 -0800

    NPC BaiYanSong

--- Assets/ShuShunRunScandinavia/Script/NPCLogic.cs
+++ Assets/ShuShunRunScandinavia/Script/NPCLogic.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class NPCLogic : MonoBehaviour
 {
+    public Rigidbody2D rigidbody;
     public AudioSource audioSource;
     public bool isReduceSanityOnTouch = true;
     public bool isReduceMoneyOnTouch = true;
@@ -11,6 +12,9 @@ public class NPCLogic : MonoBehaviour
     public int moneyDamagePerSecond = 100;
     public bool canDoDamage = true;
     public bool isHavingSpeech = false;
+    public float moveSpeed = 5f;
+    public bool isFollowingPlayer = false;
+    public float playerDistanceEpsilon = 0.5f; // Mininum distance to chase the player
     public AudioClip[] speechClips;
     public GameObject[] speechDisplays; // Sentence the NPC says when the player touches him
     public Transform speechPos;
@@ -20,7 +24,7 @@ public class NPCLogic : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            audioSource = GetComponent<AudioSource>();
+            
             if (speechDisplays.Length > 0 && !isHavingSpeech)
             {
                 StartCoroutine(ShowSpeech());
@@ -84,12 +88,27 @@ public class NPCLogic : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isFollowingPlayer)
+        {
+            Vector3 relativeVec = transform.position - GameplayManager.instance.player.transform.position;
+            if (Mathf.Abs(relativeVec.x) > playerDistanceEpsilon)
+            {
+                if (relativeVec.x > 0) //player is on the left side
+                {
+                    rigidbody.MovePosition((Vector2)transform.position - Vector2.right * moveSpeed * Time.deltaTime);
+                }
+                else
+                {
+                    rigidbody.MovePosition((Vector2)transform.position + Vector2.right * moveSpeed * Time.deltaTime);
+                }
+            }            
+        }
     }
 }
